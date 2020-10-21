132850e957cc9731a232740dd0b055f8b93a6cd6
pranjul8181 <il>
2020-10-21 20:44:50 +0530

    Week2 py flight

---
+++ week2/classes.py
@@ -0,0 +1,32 @@
+#class Point():
+#   def __init__(self, input1, input2):
+#       self.x = input1
+#       self.y = input2
+#p = Point(2,8)
+#print(p.x)
+#print(p.y) 
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+    
+    def add_passeneger(self,name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+people = ["Harry", "Ron","Hermion","Ginny"]
+for person in people:
+    success = flight.add_passeneger(person)
+    if success:
+        print(f"Added {person} to flight successfully.")
+    else:
+        print(f"No available seats for {person}")
+
+
